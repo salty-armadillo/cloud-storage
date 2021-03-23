a2270fa3a13f79424a761ecd6a6ea6c6a5391641
Kaman Chan <kamanchan27@gmail.com>
2021-03-23 22:51:11 +1100

    typo fix

--- file-manager/src/routes/upload.py
+++ file-manager/src/routes/upload.py
@@ -7,7 +7,7 @@ UPLOAD = Blueprint('upload', __name__)
 
 @UPLOAD.route('/upload', methods=['POST'])
 def upload_file():
-    '''Download file given filename'''
+    '''Upload file given filename'''
     filename = request.args.get("filename")
     response = get_presigned_url("enc-bucket-6841", filename)
     return response
