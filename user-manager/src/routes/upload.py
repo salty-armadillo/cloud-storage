615f978bc07337b9f76ab9428ccab2d87e199059
Kaman Chan <kamanchan27@gmail.com>
2021-03-23 22:52:31 +1100

    error handling fix - remove unneccessary condition

--- user-manager/src/routes/upload.py
+++ user-manager/src/routes/upload.py
@@ -1,13 +1,13 @@
 import json
 from flask import Blueprint, request
 from jsonschema import validate
-from services.upload import get_presigned_url
+from services.upload import post_file
 
 UPLOAD = Blueprint('upload', __name__)
 
 @UPLOAD.route('/upload', methods=['POST'])
 def upload_file():
-    '''Download file given filename'''
-    filename = request.args.get("filename")
-    response = get_presigned_url("enc-bucket-6841", filename)
-    return response
+    '''Upload file given filename'''
+    filepath = request.args.get("filepath")
+    post_file(filepath)
+    return json.dumps({})
